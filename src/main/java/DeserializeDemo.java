feafdb3b4dce33af23d03c4aefb172c1aa445c90
hkl <2676088581@qq.com>
2022-11-27 23:05:41 +0800

    qqq

---
+++ src/main/java/DeserializeDemo.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class DeserializeDemo {
+    public static void main(String [] args)
+    {
+        Employee e = null; //e为null 对e进行赋值
+        try
+        {
+            FileInputStream fileIn = new FileInputStream("D:\\employee.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            e = (Employee) in.readObject();  //读取对象 转换为对象e
+            in.close(); //对象流关闭
+            fileIn.close(); //IO流关闭
+        }catch(IOException i)
+        {
+            i.printStackTrace();
+            return;
+        }catch(ClassNotFoundException c)
+        {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return;
+        }
+        System.out.println("Deserialized Employee...");
+        System.out.println("Name: " + e.name);
+        System.out.println("Address: " + e.address);
+        System.out.println("SSN: " + e.SSN);
+        System.out.println("Number: " + e.number);  //输出对象的属性信息 对象的属性信息保存在内存中
+    }
+}
